8d8240ffc8223e18135fd72cbc852898cb8eff26
Paulo Eduardo Konopka <55335712+PewDizinho@users.noreply.github.com>
2025-06-01 17:30:06 -0300

    Update server.js
    Agora aceita /npcs

--- server.js
+++ server.js
@@ -65,6 +65,32 @@ app.get("/players", (req, res) => { //Quando eu acesso o 192.168.1.15:6060/playe
     });
 });
 
+app.get("/npcs", (req, res) => { //Quando eu acesso o 192.168.1.15:6060/npcs ele vai me retornar todos os players salvos na pasta /public/db/Npc
+    //Eu faço isso pra conseguir ter uma lista da pasta, é uma forma mais fácil de resolver um outro problema que eu tive
+    const directoryPath = __dirname + "\\public\\db\\Npc";
+
+    fs.readdir(directoryPath, (err, files) => {
+        if (err) { //Caso dê erro
+            console.error("Error reading directory", err);
+            res.statusCode = 500;
+            return res.end("Error reading directory");
+        }
+        //Isso resolve um erro chamado CORS, pesquisem se quiserem saber mais
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        res.setHeader("Content-Type", "text/plain");
+
+
+        const fileContents = files.map(file => {//Ele lê os arquivos e salva dentro de um array
+            const filePath = `${directoryPath}\\${file}`;
+            return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+        });
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify(fileContents));//Devolve o nome dos arquivos dentro da pasta /public/db/Player
+    });
+});
+
 
 
 
