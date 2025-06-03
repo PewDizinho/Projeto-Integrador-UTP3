eb922114918c2cb6f3750ee5b5df7c27d51b909b
eduardahor <eduardahorningxz@hotmail.com>
2025-06-03 11:32:00 -0300

    Teste

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => { //Rota principal, ou seja, no 192.168.1.15:6060/ el
 });
 
 
-
+//162.168.0.1:6060/players
 app.post("/api", (req, res) => {//Quando eu dou um POST ou seja, envio informações para o site no 192.168.1.15:6060/api 
     let body = "";
     req.on("data", chunk => {
@@ -42,6 +42,32 @@ app.post("/api", (req, res) => {//Quando eu dou um POST ou seja, envio informaç
 app.get("/players", (req, res) => { //Quando eu acesso o 192.168.1.15:6060/players ele vai me retornar todos os players salvos na pasta /public/db/Player
     //Eu faço isso pra conseguir ter uma lista da pasta, é uma forma mais fácil de resolver um outro problema que eu tive
     const directoryPath = __dirname + "\\public\\db\\Player";
+
+    fs.readdir(directoryPath, (err, files) => {
+        if (err) { //Caso dê erro
+            console.error("Error reading directory", err);
+            res.statusCode = 500;
+            return res.end("Error reading directory");
+        }
+        //Isso resolve um erro chamado CORS, pesquisem se quiserem saber mais
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        res.setHeader("Content-Type", "text/plain");
+
+
+        const fileContents = files.map(file => {//Ele lê os arquivos e salva dentro de um array
+            const filePath = `${directoryPath}\\${file}`;
+            return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+        });
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify(fileContents));//Devolve o nome dos arquivos dentro da pasta /public/db/Player
+    });
+});
+
+app.get("/npcs", (req, res) => { //Quando eu acesso o 192.168.1.15:6060/npcs ele vai me retornar todos os players salvos na pasta /public/db/Npc
+    //Eu faço isso pra conseguir ter uma lista da pasta, é uma forma mais fácil de resolver um outro problema que eu tive
+    const directoryPath = __dirname + "\\public\\db\\Npc";
 
     fs.readdir(directoryPath, (err, files) => {
         if (err) { //Caso dê erro
